9101faffa4d5d66ea8cb669db57bd214e41b23f7
mohcinebadour2000 <79023454+mohcinebadour2000@users.noreply.github.com>
2023-03-08 15:48:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ function arithmetic_operation($num1, $num2, $operator) {
 }
 
 function test_arithmetic_operation() {
-    assert(arithmetic_operation(2, 1, '+') == 5);
+    assert(arithmetic_operation(2, 3, '+') == 5);
     assert(arithmetic_operation(2, 3, '-') == -1);
     assert(arithmetic_operation(2, 3, '*') == 6);
 }
